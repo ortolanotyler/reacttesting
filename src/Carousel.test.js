7332bee2b6cec9a64150db0a4119661dcdadbdd6
Tyler Ortolano <111775236+ortolanotyler@users.noreply.github.com>
2023-11-13 15:28:55 -0500

    Create Carousel.test.js

---
+++ src/Carousel.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+
+it("works when you click on the right arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
